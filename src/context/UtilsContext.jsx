b2b1f66b9791f83021b73ad8f549e48a8e2ffbd8
Krupa22 <krupakaklotar22@gmail.com>
2024-10-28 15:32:26 +0530

    Done

---
+++ src/context/UtilsContext.jsx
@@ -0,0 +1,24 @@
+import {useState,useContext,createContext} from "react";
+
+const UtilsContext = createContext(null);
+
+export const UtilsContextProvider = ({ children }) => {
+    const [isSidebar, setIsSidebar] = useState(false);
+    const [mobileShow, setMobileShow] = useState(false);
+
+    return (
+        <UtilsContext.Provider value={{ isSidebar, setIsSidebar, mobileShow, setMobileShow }}>
+            {children}
+        </UtilsContext.Provider>
+    );
+};
+
+export const useUtils = () => {
+    const context = useContext(UtilsContext);
+
+    if (!context) {
+        throw new Error("useUtils must be used within a UtilsContextProvider");
+    }
+
+    return context;
+};
